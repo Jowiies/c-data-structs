2ff4bde398657987793cd9775e547bfafa4b5083
Jowiies <joel.esteller@estudiantat.upc.edu>
2024-10-16 23:48:07 +0200

    correcting indentation

--- dictionarycopy/dictionary.c
+++ dictionarycopy/dictionary.c
@@ -5,172 +5,163 @@
 #include <string.h>
 #include <ctype.h>
 
-//=========================================================================================
-//===============================ALLOCATION FUNCTIONS======================================
-//=========================================================================================
+/* ALLOCATION FUNCTIONS */
 
 Node *newNode(const char c, Node *child, Node *brother) 
 {
-    Node *node = (Node*)malloc(sizeof(Node));
-    
-    if (node == NULL) {
-        fprintf(stderr, "Memory allocation error: Couldn't create a new node");
-        exit(1);  
-    }
-    
-    node->_brother = brother;
-    node->_child = child;
-    node->_key = (c != ENDCHAR) ? tolower(c) : c;
-    node->_wordcount = (c == ENDCHAR) ? 1 : 0;
-    
-    return node;
+        Node *node = (Node*)malloc(sizeof(Node));
+
+        if (node == NULL) {
+                fprintf(stderr, "Memory allocation error: Couldn't create a new node");
+                exit(1);  
+        }
+
+        node->_brother = brother;
+        node->_child = child;
+        node->_key = (c != ENDCHAR) ? tolower(c) : c;
+        node->_wordcount = (c == ENDCHAR) ? 1 : 0;
+
+        return node;
 }
 
 Dictionary newDictionary()
 {
-   Dictionary d = (Dictionary)malloc(sizeof(Dictionary_t));
-   
-   if (!d) {
-       fprintf(stderr, "Memory allocation error: Couldn't create the dictionary");
-       exit(1);  
-   }
-   
-   d->_firstnode = NULL;
-   d->_uniqueCount = 0;
-   
-   return d;
+        Dictionary d = (Dictionary)malloc(sizeof(Dictionary_t));
+
+        if (!d) {
+                fprintf(stderr, "Memory allocation error: Couldn't create the dictionary");
+                exit(1);  
+        }
+
+        d->_firstnode = NULL;
+        d->_uniqueCount = 0;
+
+        return d;
 }
 
-//=========================================================================================
-//================================DEALLOCATION FUNCTIONS===================================
-//=========================================================================================
+/* DEALLOCATION FUNCTIONS */
 
 void freeNodes(Node *ptr)
 {
-    if (ptr == NULL)
-        return;
-    
-    freeNodes(ptr->_child);
-    freeNodes(ptr->_brother);
-    free(ptr);
+        if (ptr == NULL)
+                return;
+
+        freeNodes(ptr->_child);
+        freeNodes(ptr->_brother);
+        free(ptr);
 }
 
 void freeDictionary(Dictionary dict)
 {
-    freeNodes(dict->_firstnode);
-    free(dict);
+        freeNodes(dict->_firstnode);
+        free(dict);
 }
 
-//=========================================================================================
-//=================================PRINT FUNCTIONS=========================================
-//=========================================================================================
+/* PRINT FUNCTIONS */
 
 void printNodes(Node *ptr, char buffer[], uint32_t *it)
 {
-    if (ptr == NULL)
-        return;
+        if (ptr == NULL)
+                return;
 
-    if (ptr->_key == ENDCHAR) {
-        buffer[*it] = '\0';
-        printf(" - %s --> %d times.\n", buffer, ptr->_wordcount);
-    }
+        if (ptr->_key == ENDCHAR) {
+                buffer[*it] = '\0';
+                printf(" - %s --> %d times.\n", buffer, ptr->_wordcount);
+        }
 
 
-    buffer[*it] = ptr->_key;
-    (*it)++;
+        buffer[*it] = ptr->_key;
+        (*it)++;
 
-    printNodes(ptr->_child, buffer, it);
-    (*it)--;
-    printNodes(ptr->_brother, buffer, it);
+        printNodes(ptr->_child, buffer, it);
+        (*it)--;
+        printNodes(ptr->_brother, buffer, it);
 }
 
 void printDictionary(Dictionary dict)
 {
-    char buffer[48] = {0};
-    uint32_t iter = 0;
-    printNodes(dict->_firstnode, buffer, &iter); 
+        char buffer[48] = {0};
+        uint32_t iter = 0;
+        printNodes(dict->_firstnode, buffer, &iter); 
 }
 
-//=========================================================================================
-//=========================================================================================
-//=========================================================================================
 
 
 //Returns a pointer to a new dynamically allocated node
 Node *insertNode(Node *ptr, const char* word, const int *size, int it, uint32_t *uwc)
 {
 
-    if (it == *size) {
-        if (ptr == NULL) {
-            ptr = newNode(ENDCHAR, NULL, NULL);
-            (*uwc)++;
+        if (it == *size) {
+                if (ptr == NULL) {
+                        ptr = newNode(ENDCHAR, NULL, NULL);
+                        (*uwc)++;
+                }
+                else if (ptr->_key == ENDCHAR) {
+                        ptr->_wordcount++;
+                }
+                else {
+                        Node *newnode = newNode(ENDCHAR, NULL, ptr);
+                        ptr = newnode;
+                        (*uwc)++;
+                }
+                return ptr;
         }
-        else if (ptr->_key == ENDCHAR) {
-            ptr->_wordcount++;
+        int c = tolower(word[it]);
+
+        if (ptr == NULL) {
+                ptr = newNode(c, NULL, NULL);
+        }
+
+        if (ptr->_key > c) {
+                Node* newnode = newNode(c, NULL, ptr);
+                ptr = newnode;
+                ptr->_child = insertNode(ptr->_child, word, size, it+1, uwc);
+        }
+        else if (ptr->_key < c) {
+                ptr->_brother = insertNode(ptr->_brother, word, size, it, uwc);
         }
         else {
-            Node *newnode = newNode(ENDCHAR, NULL, ptr);
-            ptr = newnode;
-            (*uwc)++;
+                ptr->_child = insertNode(ptr->_child, word, size, it+1, uwc);
         }
+
         return ptr;
-    }
-    int c = tolower(word[it]);
-
-    if (ptr == NULL) {
-        ptr = newNode(c, NULL, NULL);
-    }
-
-    if (ptr->_key > c) {
-        Node* newnode = newNode(c, NULL, ptr);
-        ptr = newnode;
-        ptr->_child = insertNode(ptr->_child, word, size, it+1, uwc);
-    }
-    else if (ptr->_key < c) {
-        ptr->_brother = insertNode(ptr->_brother, word, size, it, uwc);
-    }
-    else {
-        ptr->_child = insertNode(ptr->_child, word, size, it+1, uwc);
-    }
-
-    return ptr;
 }
 
 
 Node* findNode(Node *ptr, const char* word, size_t* size, size_t it)
 {
-    if (ptr != NULL) {
-        char c = tolower(word[it]);
-        if (it == *size)
-            return (ptr->_key == ENDCHAR) ? ptr : NULL;
-        
-        if (ptr->_key < c)
-            return findNode(ptr->_brother, word, size, it);
-        
-        if (ptr->_key == c)
-            return findNode(ptr->_child, word, size, it+1);
-    }
-    
-    return NULL;
+        if (ptr != NULL) {
+                char c = tolower(word[it]);
+                if (it == *size)
+                        return (ptr->_key == ENDCHAR) ? ptr : NULL;
+
+                if (ptr->_key < c)
+                        return findNode(ptr->_brother, word, size, it);
+
+                if (ptr->_key == c)
+                        return findNode(ptr->_child, word, size, it+1);
+        }
+
+        return NULL;
 }
 
 void dictionaryInsert(Dictionary dict, const char* word)
 {
-    if (word == NULL) {
-        fprintf(stderr, "Insertion Error: The word was NULL");
-        exit(1);
-    }
-    dict->_totalwords++;
-    int wordsize = strlen(word), iter = 0;
-    dict->_firstnode = insertNode(dict->_firstnode, word, &wordsize, iter, &dict->_uniqueCount);
+        if (word == NULL) {
+                fprintf(stderr, "Insertion Error: The word was NULL");
+                exit(1);
+        }
+        dict->_totalwords++;
+        int wordsize = strlen(word), iter = 0;
+        dict->_firstnode = insertNode(dict->_firstnode, word, &wordsize, iter, &dict->_uniqueCount);
 }
 
 uint32_t getUniqueWordCount(Dictionary dict)
 {
-    return dict->_uniqueCount;
+        return dict->_uniqueCount;
 }
 
 uint32_t getTotalWords(Dictionary dict)
 {
-    return dict->_totalwords;
+        return dict->_totalwords;
 }
--- stack/Stack.c
+++ stack/Stack.c
@@ -4,74 +4,74 @@
 
 Stack* newStack() 
 {
-    Stack *stk = (Stack*)malloc(sizeof(Stack));
-    if (!stk) {
-        fprintf(stderr, "Memory allocation error\n");
-        exit(-1);
-    }
-    stk->_size = 0;
-    stk->_topItem = NULL;
-    return stk;
+        Stack *stk = (Stack*)malloc(sizeof(Stack));
+        if (!stk) {
+                fprintf(stderr, "Memory allocation error\n");
+                exit(-1);
+        }
+        stk->_size = 0;
+        stk->_topItem = NULL;
+        return stk;
 }
 
 void stackPush(Stack* stk, int value)
 {
-    Item *ptr = (Item*)malloc(sizeof(Item));
-    if (!ptr) {
-        fprintf(stderr, "Memory allocation error\n");
-        exit(-1);
-    }
-    ptr->_value = value;
-    ptr->_next = stk->_topItem;
-    stk->_topItem = ptr;
-    stk->_size++;
+        Item *ptr = (Item*)malloc(sizeof(Item));
+        if (!ptr) {
+                fprintf(stderr, "Memory allocation error\n");
+                exit(-1);
+        }
+        ptr->_value = value;
+        ptr->_next = stk->_topItem;
+        stk->_topItem = ptr;
+        stk->_size++;
 }
 
 int stackPop(Stack* stk)
 {
-    if (!stk) {
-        fprintf(stderr, "Error: the stack was null\n");
-        exit(-1);
-    }
-    if (stk->_size == 0) {
-        fprintf(stderr, "Error: can't pop a void stack\n");
-        exit(-1);
-    }
-    if (!stk->_topItem) {
-        fprintf(stderr, "Error: the Item was null\n");
-        exit(-1);
-    }
+        if (!stk) {
+                fprintf(stderr, "Error: the stack was null\n");
+                exit(-1);
+        }
+        if (stk->_size == 0) {
+                fprintf(stderr, "Error: can't pop a void stack\n");
+                exit(-1);
+        }
+        if (!stk->_topItem) {
+                fprintf(stderr, "Error: the Item was null\n");
+                exit(-1);
+        }
 
-    int value = stk->_topItem->_value;
-    Item* deleteptr = stk->_topItem;
-    stk->_topItem = stk->_topItem->_next;
-    free(deleteptr);
-    stk->_size--;
-    return value;
+        int value = stk->_topItem->_value;
+        Item* deleteptr = stk->_topItem;
+        stk->_topItem = stk->_topItem->_next;
+        free(deleteptr);
+        stk->_size--;
+        return value;
 }
 
 void itemsDelete(Item* it)
 {
-    if (it == NULL)
-        return;
-    itemsDelete(it->_next);
-    free((Item*)it);
+        if (it == NULL)
+                return;
+        itemsDelete(it->_next);
+        free((Item*)it);
 }
 
 void stackDelete(Stack* stk)
 {
-    itemsDelete(stk->_topItem);
-    free(stk);
+        itemsDelete(stk->_topItem);
+        free(stk);
 }
 
 void stackPrint(Stack* stk) 
 {
-    Item* ptr = stk->_topItem;
+        Item* ptr = stk->_topItem;
 
-    while (ptr != NULL) {
-        printf("%d ", ptr->_value);
-        ptr = ptr->_next;
-    }
+        while (ptr != NULL) {
+                printf("%d ", ptr->_value);
+                ptr = ptr->_next;
+        }
 
-    printf("]\n size: %d\n",(int)stk->_size);
+        printf("]\n size: %d\n",(int)stk->_size);
 }
