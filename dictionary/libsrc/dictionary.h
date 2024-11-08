ea937d63e4b9cd62049c8d880074d8e86611c4fc
jowiees <jowiees@10-192-180-255client.eduroam.upc.edu>
2024-11-08 11:37:35 +0100

    some comments

--- dictionary/libsrc/dictionary.h
+++ dictionary/libsrc/dictionary.h
@@ -19,11 +19,21 @@ typedef struct Dictionary_t {
 
 typedef Dictionary_t* Dictionary;
 
+// retuns a pointer to an allocated void dictionary
 Dictionary newDictionary();
+
+// inserts a new word + description to the dictionary.
 void dictionaryInsert(Dictionary dict, const char* word, const char* description);
+
+// return a strin_view of the description of the word.
 const char* dictionarySearch(Dictionary dict, const char* word);
+
+// removes the word from the dictionary.
 void dictionaryRemoveWord(Dictionary dict, const char* word);
+
+// returns how many word start with str.
 size_t dictionaryContains(Dictionary dict, const char* str);
+
 void freeDictionary(Dictionary dict);
 
 #endif
